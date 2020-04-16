2da612f9edec5b3fc7ea4bc779625d21c9cdbee2
ddmbb <d.baker089@gmail.com>
2020-04-16 00:27:08 -0400

    formatting

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 
-const numDogs = $('#dogs').val();
+const numDogs = $("#dogs").val();
 
 function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random/numDogs')
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+  fetch("https://dog.ceo/api/breeds/image/random/numDogs")
+    .then((response) => response.json())
+    .then((responseJson) => console.log(responseJson));
 }
 
 function watchForm() {
-  $('form').submit(event => {
+  $("form").submit((event) => {
     event.preventDefault();
     getDogImage();
   });
 }
 
-$(function() {
-  console.log('App loaded! Waiting for submit!');
+$(function () {
+  console.log("App loaded! Waiting for submit!");
   watchForm();
 });
